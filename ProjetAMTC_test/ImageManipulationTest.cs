d539afd47adc6902d1ed353c046eb9b8e77c1b93
amich <amichelet95@gmail.com>
2023-11-04 11:52:16 +0100

    ProjetAMTC_test creation

---
+++ ProjetAMTC_test/ImageManipulationTest.cs
@@ -0,0 +1,11 @@
+namespace ProjetAMTC_test
+{
+    [TestClass]
+    public class ImageManipulationTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
